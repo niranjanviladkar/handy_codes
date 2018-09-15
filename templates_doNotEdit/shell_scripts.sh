022bfebcf211f340f331e5ed75aa2b0bc0cebfbc
Niranjan Viladkar <nviladkar@gmail.com>
2018-09-15 15:51:37 +0530

    Removing the file as the content moved to bashrc.

--- templates_doNotEdit/shell_scripts.sh
+++
@@ -1,6 +0,0 @@
-# to check continuous mem usage
-while [[ 1 ]]; do free -h | tail -n2 | head -n1; sleep 1; done
-
-# Go to a top level directory - parent for many git repos
-# run below code to get all repos updated in one go.
-for dir in `ls -1`; do echo -n "$dir "; cd $dir; git pull; cd ..; done
